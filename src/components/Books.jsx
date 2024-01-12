e72a18bb4cd18ef99401284e4c55dcae15b7e79c
Ali Ghaffari <aligh71@yahoo.com>
2024-01-12 12:23:56 +0330

    adding liked books to your favorite list

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 import { books } from "../constants/mockData";
 import BookCard from "./BookCard";
 
 function Books() {
+  const [liked, setLiked] = useState([]);
+  const handleLikedList = (book, status) => {
+    if (status) {
+      const newLikedList = liked.filter((i) => i.id !== book.id);
+      setLiked(newLikedList);
+    } else {
+      setLiked((liked) => [...liked, book]);
+    }
+  };
   return (
     <div>
       <div>
         {books.map((book) => (
-          <BookCard key={book.id} data={book} />
+          <BookCard
+            key={book.id}
+            data={book}
+            handleLikedList={handleLikedList}
+          />
         ))}
       </div>
-      <div></div>
+      {!!liked.length && <div>ss</div>}
     </div>
   );
 }
