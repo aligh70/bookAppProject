5fe8466781a9f3bc398bd6bbc8563649228aba42
Ali Ghaffari <aligh71@yahoo.com>
2024-01-12 11:50:47 +0330

    the possibility of liking the post by the user and solving the problem of not reloading the photos

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import { AiFillHeart } from "react-icons/ai";
+
+import styles from "./BookCard.module.css";
+
 function BookCard({ data: { title, author, image, language, pages } }) {
+  const [like, setLike] = useState(false);
+  const likeHandler = () => {
+    setLike((like) => !like);
+  };
   return (
-    <div>
+    <div className={styles.card}>
       <img src={image} alt={title} />
-      <div>
+      <div className={styles.info}>
         <h3>{title}</h3>
         <p>{author}</p>
         <div>
@@ -10,7 +19,9 @@ function BookCard({ data: { title, author, image, language, pages } }) {
           <span>{pages} pages</span>
         </div>
       </div>
-      <button>like</button>
+      <button onClick={likeHandler}>
+        <AiFillHeart color={like ? "red" : "#e0e0e0"} fontSize="1.8rem" />
+      </button>
     </div>
   );
 }
