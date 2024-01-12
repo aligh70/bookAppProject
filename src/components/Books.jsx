12605797cbd8140741147777a70efd265dcde275
Ali Ghaffari <aligh71@yahoo.com>
2024-01-12 12:54:03 +0330

    show favorites list

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import { books } from "../constants/mockData";
 import BookCard from "./BookCard";
+import SideCard from "./SideCard";
+
+import styles from "./Books.module.css";
 
 function Books() {
   const [liked, setLiked] = useState([]);
@@ -14,8 +17,8 @@ function Books() {
     }
   };
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.cards}>
         {books.map((book) => (
           <BookCard
             key={book.id}
@@ -24,7 +27,14 @@ function Books() {
           />
         ))}
       </div>
-      {!!liked.length && <div>ss</div>}
+      {!!liked.length && (
+        <div className={styles.favorite}>
+          <h4>Favorites</h4>
+          {liked.map((book) => (
+            <SideCard key={book.id} data={book} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
